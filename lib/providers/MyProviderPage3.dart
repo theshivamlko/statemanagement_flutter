89b7d53e887fe61d6bbdcfbdf2b93f02e2898aa1
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2023-09-05 23:12:13 +0530

    context.watch and context.read

--- lib/providers/MyProviderPage3.dart
+++ lib/providers/MyProviderPage3.dart
@@ -14,19 +14,16 @@ class MyProviderPage3 extends StatefulWidget {
 }
 
 class _MyProviderPage3State extends State<MyProviderPage3> {
-  late MyBlocNotifierWithParam myBlocNotifier;
+ // late MyBlocNotifierWithParam myBlocNotifier;
 
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
-    myBlocNotifier = Provider.of<MyBlocNotifierWithParam>(context);
-    MyBlocNotifier2 bloc = Provider.of<MyBlocNotifier2>(context);
-
-    print("MyProviderPage3 build  ${myBlocNotifier.appState.hashCode}  ${bloc.appState.hashCode}");
+   // myBlocNotifier = Provider.of<MyBlocNotifierWithParam>(context);
+  //  MyBlocNotifier2 bloc = context.watch<MyBlocNotifier2>();
+    MyBlocNotifier2 bloc2=  context.read<MyBlocNotifier2>();
+ //   print(
+//        "MyProviderPage3 build  ${myBlocNotifier.hashCode}  ${bloc.hashCode}  ${bloc2.hashCode}");
 
     return Scaffold(
       appBar: AppBar(
@@ -39,19 +36,42 @@ class _MyProviderPage3State extends State<MyProviderPage3> {
           children: <Widget>[
             GestureDetector(
               onTap: () {
-                myBlocNotifier.increment();
+           //     myBlocNotifier.increment();
               },
               child: Text(
-                'Proxy ${myBlocNotifier.appState?.counter} \n '
-                    'Bloc1 ${bloc.appState?.counter} \n ',
+            //    'Proxy ${myBlocNotifier.appState?.counter} \n '
+                'Bloc1 ${bloc2.appState?.counter} \n '
+                ,
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
             ),
-            ElevatedButton(onPressed: () {
-              bloc.increment();
-            }, child: Text("Counter")),
+            ElevatedButton(
+                onPressed: () {
+                  bloc2.increment();
+                },
+                child: Text("Counter")),
             Padding(padding: EdgeInsets.all(20)),
-            ChildWidget()
+            GestureDetector(
+              onTap: () {
+                bloc2.increment();
+              },
+              child: Text(
+                'context.watch<>',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            GestureDetector(
+              onTap: () {
+                bloc2.increment();
+              },
+              child: Text(
+                'context.read<>',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            Padding(padding: EdgeInsets.all(20)),
+            GestureDetector(onTap: () {}, child: ChildWidget())
           ],
         ),
       ),
@@ -60,22 +80,19 @@ class _MyProviderPage3State extends State<MyProviderPage3> {
 }
 
 class ChildWidget extends StatelessWidget {
-
   Widget build(BuildContext context) {
-    MyBlocNotifierWithParam myBlocNotifier = Provider.of<MyBlocNotifierWithParam>(context);
+    MyBlocNotifier2 bloc2=  context.read<MyBlocNotifier2>();
+
 
     return Column(
       children: [
         GestureDetector(
             onTap: () {},
             child: Text(
-              'BLOC1 State ${myBlocNotifier?.appState.counter}',
+              'BLOC1 State ${bloc2?.appState.counter}',
               style: TextStyle(fontSize: 20),
             )),
-
       ],
     );
   }
-
-
 }
